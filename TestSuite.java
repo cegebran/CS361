3c8361cc30f35055f35e10baae05e8d1adf4c500
Brandon Cegelski <Brandon Cegelski>
2017-02-02 15:18:30 -0600

    test suite file to run both test files together

---
+++ TestSuite.java
@@ -0,0 +1,14 @@
+package scoresheet;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith (Suite.class)
+@Suite.SuiteClasses({
+	ScoreSheetTest1.class,
+	ScoreSheetTest2.class })
+
+public class TestSuite {
+
+}
