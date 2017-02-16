6b88694d41c923e6eb08cceaadf2732bd1e57a07
Brandon Cegelski <Brandon Cegelski>
2017-02-16 13:36:28 -0600

    Taylor's skeleton he created and could not upload

---
+++ Chronotimer/src/ChronoTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ChronoTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Chronotimer/src/TestSuite.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestSuite {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
