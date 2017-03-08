8e77af67572ddf0a40e4faf2ce12e48eb8ce701d
soderli2 <soderli2@uwm.edu>
2017-03-08 13:46:48 -0600

    I added MainDirectory.java!!!!!!!! :)

---
+++ Lab07/src/MainDirectory.java
@@ -0,0 +1,11 @@
+
+
+public class MainDirectory {
+	
+	// implement adding from GSON into collection
+	
+	// implement print : IOW, send data via JSON
+	
+	// implement clear (CLR), delete all contacts in directory
+	
+}
