4daa7e70c8610b8c6b5bb5e12001b9ba612b021b
Nathan <wenslaff@uwm.edu>
2017-03-19 20:51:06 -0500

    finished client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -11,99 +11,249 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-public class Client extends JFrame implements ActionListener{
-//test	
+public class Client extends JFrame{
+static String fname = "";
+static String lname = "";
+static String departmentString = "";
+static String phoneString = "";
+static String titleString = "";
+static String genderString = "";
+
 	public static void main(String[] args) {
-		try {
-			Client myGUI = new Client();
-			myGUI.setVisible(true);
-			
-			System.out.println("in the client");
-
-			// Client will connect to this location
-			URL site = new URL("http://localhost:8000/sendresults");
-			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
-
-			// now create a POST request
-			conn.setRequestMethod("POST");
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-
-			// build a string that contains JSON from console
-			String content = "";
-			content = getJSON();
-
-			// write out string to output buffer for message
-			out.writeBytes(content);
-			out.flush();
-			out.close();
-
-			System.out.println("Done sent to server");
-
-			InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
-
-			// string to hold the result of reading in the response
-			StringBuilder sb = new StringBuilder();
-
-			// read the characters from the request byte by byte and build up
-			// the Response
-			int nextChar;
-			while ((nextChar = inputStr.read()) > -1) {
-				sb = sb.append((char) nextChar);
-			}
-			System.out.println("Return String: " + sb);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static String getJSON() {
-
-		ArrayList<Employee> em = new ArrayList<>();
 		
-		Gson g = new Gson();
-		String json = g.toJson(em);
-		return json;
+		Client myGUI = new Client();
+		myGUI.setVisible(true);
+		System.out.println("in the client");
+	
 	}
-
 	public Client(){//Making a GUI out of a constructor
-		JTextField firstName = new JTextField("", 15);
-		JTextField lastName = new JTextField("", 15);
-		JTextField department = new JTextField("", 15);
-		JTextField phone = new JTextField("", 15);
+		final JTextField firstName = new JTextField("", 15);
+		firstName.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		        System.out.println("First Name=" + firstName.getText());
+		        fname = firstName.getText();
+		      }
+		});
+		final JTextField lastName = new JTextField("", 15);
+		lastName.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		        System.out.println("Last Name=" + lastName.getText());
+		        lname = lastName.getText();
+		      }
+		});
+		final JTextField department = new JTextField("", 15);
+		department.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		        System.out.println("Department=" + department.getText());
+		        departmentString = department.getText();
+		      }
+		});
+		final JTextField phone = new JTextField("", 15);
+		phone.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		        System.out.println("Phone=" + phone.getText());
+		        phoneString = phone.getText();
+		      }
+		});
 		JRadioButton genderMale = new JRadioButton("Male");
+		genderMale.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		    	  System.out.println("Gender = male");
+		    	  genderString = "male";
+		      }
+		});
 		JRadioButton genderFemale = new JRadioButton("Female");
+		genderFemale.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		    	  System.out.println("Gender = female");
+		    	  genderString = "female";
+		      }
+		});
 		JRadioButton genderOther = new JRadioButton("Other");
+		genderOther.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		    	  System.out.println("Gender = other");
+		    	  genderString = "other";
+		      }
+		});
+		ButtonGroup group = new ButtonGroup();
+		group.add(genderMale);
+		group.add(genderFemale);
+		group.add(genderOther);
 		String[] listOfTitle = {"Mr.", "Ms", "Mrs.", "Dr.", "Col.", "Prof.", };
-		JList title = new JList(listOfTitle);
+		final JList title = new JList(listOfTitle);
+		title.addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent e){
+			    if (e.getKeyCode() == KeyEvent.VK_ENTER){
+			           titleString = (String) title.getSelectedValue();	
+			           System.out.print("Title = " + titleString);
+			    }
+			}
+			});
 		title.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		title.setLayoutOrientation(JList.HORIZONTAL_WRAP);
 		title.setVisibleRowCount(-1);  
 //		JScrollPane listScroller = new JScrollPane(listOfTitle);
 //		listScroller.setPreferredSize(new Dimension(250, 80));
-		JButton submit = new JButton("Submit");
-		JButton exit = new JButton("Exit");
-		
+		final JButton add = new JButton("Add");
+		add.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e){
+	        	try {
+	    			// Client will connect to this location
+	    			URL site = new URL("http://localhost:8000/sendresults");
+	    			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
+
+	    			// now create a POST request
+	    			conn.setRequestMethod("POST");
+	    			conn.setDoOutput(true);
+	    			conn.setDoInput(true);
+	    			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+	    			// build a string that contains JSON from console
+	    			String content = "";
+	    			content = "ADD;" + getJSON();
+
+	    			// write out string to output buffer for message
+	    			out.writeBytes(content);
+	    			out.flush();
+	    			out.close();
+
+	    			System.out.println("Done sent to server");
+
+	    			InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
+
+	    			// string to hold the result of reading in the response
+	    			StringBuilder sb = new StringBuilder();
+
+	    			// read the characters from the request byte by byte and build up
+	    			// the Response
+	    			int nextChar;
+	    			while ((nextChar = inputStr.read()) > -1) {
+	    				sb = sb.append((char) nextChar);
+	    			}
+	    			System.out.println("Return String: " + sb);
+
+	    		} catch (Exception e2) {
+	    			e2.printStackTrace();
+	    		}
+	        	
+	        	
+	            Container frame = add.getParent();
+	            do 
+	                frame = frame.getParent(); 
+	            while (!(frame instanceof JFrame));                                      
+	            ((JFrame) frame).dispose();
+	        }
+	    });
+		JButton print = new JButton("Print");
+		print.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e){
+	        	try {
+	    			// Client will connect to this location
+	    			URL site = new URL("http://localhost:8000/sendresults");
+	    			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
+
+	    			// now create a POST request
+	    			conn.setRequestMethod("POST");
+	    			conn.setDoOutput(true);
+	    			conn.setDoInput(true);
+	    			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+	    			// build a string that contains JSON from console
+	    			String content = "PRINT";
+
+	    			// write out string to output buffer for message
+	    			out.writeBytes(content);
+	    			out.flush();
+	    			out.close();
+
+	    			System.out.println("Done sent to server");
+
+	    			InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
+
+	    			// string to hold the result of reading in the response
+	    			StringBuilder sb = new StringBuilder();
+
+	    			// read the characters from the request byte by byte and build up
+	    			// the Response
+	    			int nextChar;
+	    			while ((nextChar = inputStr.read()) > -1) {
+	    				sb = sb.append((char) nextChar);
+	    			}
+	    			System.out.println("Return String: " + sb);
+
+	    		} catch (Exception e2) {
+	    			e2.printStackTrace();
+	    		}
+	        	
+	        	
+	            Container frame = add.getParent();
+	            do 
+	                frame = frame.getParent(); 
+	            while (!(frame instanceof JFrame));                                      
+	            ((JFrame) frame).dispose();
+	        }
+	    });
+		JButton clear = new JButton("Clear");
+		clear.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e){
+	        	try {
+	    			// Client will connect to this location
+	    			URL site = new URL("http://localhost:8000/sendresults");
+	    			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
+
+	    			// now create a POST request
+	    			conn.setRequestMethod("POST");
+	    			conn.setDoOutput(true);
+	    			conn.setDoInput(true);
+	    			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+	    			// build a string that contains JSON from console
+	    			String content = "CLEAR";
+
+	    			// write out string to output buffer for message
+	    			out.writeBytes(content);
+	    			out.flush();
+	    			out.close();
+
+	    			System.out.println("Done sent to server");
+
+	    			InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
+
+	    			// string to hold the result of reading in the response
+	    			StringBuilder sb = new StringBuilder();
+
+	    			// read the characters from the request byte by byte and build up
+	    			// the Response
+	    			int nextChar;
+	    			while ((nextChar = inputStr.read()) > -1) {
+	    				sb = sb.append((char) nextChar);
+	    			}
+	    			System.out.println("Return String: " + sb);
+
+	    		} catch (Exception e2) {
+	    			e2.printStackTrace();
+	    		}
+	        	
+	        	
+	            Container frame = add.getParent();
+	            do 
+	                frame = frame.getParent(); 
+	            while (!(frame instanceof JFrame));                                      
+	            ((JFrame) frame).dispose();
+	        }
+	    });
+				
 		setTitle("Lab08");
-		setSize(400, 400);
+		setSize(700, 500);
 		setLocation(10, 200);
 		setLayout(new FlowLayout());
-		
-	//		myFrame.addWindowListener(new WindowAdapter(){
-	//			public void windowClosing(WindowEvent e){
-	//				System.exit(0);
-	//			}
-	//		});
-				
-	//		submit.addActionListener(new MyActionListener());
-	//		exit.addActionListener(new MyActionListener());
-		
 		JPanel myPanel1 = new JPanel();
 		myPanel1.setLayout(new GridLayout(0, 2));
 		myPanel1.add(new JLabel("First Name"));
@@ -123,22 +273,29 @@ public class Client extends JFrame implements ActionListener{
 		myPanel2.add(genderMale);
 		myPanel2.add(genderFemale);
 		myPanel2.add(genderOther);
+		myPanel1.add(myPanel2);
 		myPanel2.setVisible(true);
 		
+		
 		JPanel myPanel3 = new JPanel();
-		myPanel3.setLayout(new GridLayout(0, 2));
-		myPanel3.add(submit);
-		myPanel3.add(exit);
-		myPanel3.setVisible(true);
+		myPanel3.setLayout(new GridLayout(0, 4));
+		myPanel3.add(add);
+		myPanel3.add(print);
+		myPanel3.add(clear);
+		myPanel1.add(myPanel3);
+		myPanel3.setVisible(true);		
 		
 		getContentPane().add(myPanel1);	
-		getContentPane().add(myPanel2);	
-		getContentPane().add(myPanel3);	
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+	private static String getJSON() {
+
+		ArrayList<Employee> em = new ArrayList<>();
+		em.add(new Employee(fname, lname, departmentString, phoneString, titleString, genderString));
+		Gson g = new Gson();
+		String json = g.toJson(em);
+		return json;
 	}
+
+	
 }
